a8409c60714614529667ed1050be68ec179d75df
Wes Oldt <wes.oldt@gmail.com>
2024-09-12 15:37:29 -0700

    add shadcn combobox example; move user search demo from app to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,5 @@
-import * as React from 'react';
-import { type ResponseMetadata, type User } from './types';
-
-type AbreviatedUserResponse = {
-  users: Pick<User, 'id' | 'firstName' | 'lastName'>[];
-} & ResponseMetadata;
+import { ComboboxDemo } from './ComboboxDemo';
 
 export default function App() {
-  const [result, setResult] = React.useState<AbreviatedUserResponse | null>(
-    null,
-  );
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const search = formData.get('search') as string;
-    const res = await fetch(
-      `https://dummyjson.com/users/search?q=${search}&limit=10&select=id,firstName,lastName`,
-    );
-    const data: AbreviatedUserResponse =
-      (await res.json()) as AbreviatedUserResponse;
-    console.log(data);
-    setResult(data);
-  };
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="search" autoComplete="off" />
-        <button type="submit">Search</button>
-      </form>
-      <hr />
-      {result && (
-        <pre className="ml-6 w-10/12 whitespace-pre-wrap">
-          {JSON.stringify(result, null, 2)}
-        </pre>
-      )}
-    </div>
-  );
+  return <ComboboxDemo />;
 }
