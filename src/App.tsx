0ea21bf285b0a8c9d0303fff7c786012ccb934d7
Wes Oldt <wes.oldt@gmail.com>
2024-09-17 20:55:38 -0700

    add more details to component demonstration

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import { UserListInfiniteScroll } from './UserListInfiniteScroll';
 export default function App() {
   return (
     <div className="flex flex-col items-center gap-2">
-      <p>No infinite scroll, search enabled, uses Command component</p>
+      <h2 className="text-2xl font-bold">Combobox with search</h2>
+      <p>No infinite scroll, search enabled, uses Command component.</p>
+      <p className="text-sm text-slate-400">
+        Can only show 10 results at time. Uses useEffect for api call.
+      </p>
       <UserCombobox />
 
       {/* divider */}
       <div className="my-4 w-9/12 border-t" />
 
-      <p>
-        With infinite scroll, no search, not using Command component (just a
-        simple list)
+      <h2 className="text-2xl font-bold">List with infinite scroll</h2>
+      <p>Uses react-window to load more items. Uses useEffect for api call.</p>
+      <p className="text-sm text-slate-400">
+        No caching, makes repeated api calls.
       </p>
       <UserListInfiniteScroll />
     </div>
