b31d730d7cad6240e24ff40db7cc5044cf4b5e28
Wes Oldt <wes.oldt@gmail.com>
2024-09-24 11:07:18 -0700

    copy cmdk source into project for better debugging and understanding

---
+++ src/components/Command/command-score.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export const commandScore = (
+  string: string,
+  abbreviation: string,
+  aliases: string[] | undefined,
+): number => {
+  return 8;
+};
